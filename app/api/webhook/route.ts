152eda6fd59ea946030e801bc75404d3993f8550
Quang Hai <haisd01@gmail.com>
2026-01-12 20:58:47 +0700

    :art: webhook clerk

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,39 @@
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+
+const webhookSecret: string = process.env.WEBHOOK_SECRET || "";
+
+export async function POST(req: Request) {
+  const svix_id = req.headers.get("svix-id") ?? "";
+  const svix_timestamp = req.headers.get("svix-timestamp") ?? "";
+  const svix_signature = req.headers.get("svix-signature") ?? "";
+  if (!webhookSecret) {
+    throw new Error("WEBHOOK_SECRET is not set");
+  }
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  const sivx = new Webhook(webhookSecret);
+
+  let msg: WebhookEvent;
+
+  try {
+    msg = sivx.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const eventType = msg.type;
+  if (eventType === "user.created") {
+    console.log("msg data", msg.data);
+  }
+  console.log("🚀 route.ts:32 - eventType:", eventType);
+
+  // Rest
+
+  return new Response("OK", { status: 200 });
+}
